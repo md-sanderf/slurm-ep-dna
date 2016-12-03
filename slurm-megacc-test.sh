0a8cbd5b1dba145fccdb215f70cdf1babfbcd5b2
md-sanderf <tuf79348@temple.edu>
2016-12-03 15:23:41 -0500

    Update slurm-megacc-test.sh

--- slurm-megacc-test.sh
+++ slurm-megacc-test.sh
@@ -8,10 +8,15 @@
 #SBATCH --mem-per-cpu=200
 
 val=$(sed "${SLURM_ARRAY_TASK_ID}q;d" /home/tuf79348/ep_env/testlist2.txt)
+echo $val
 mao=/home/tuf79348/ep_env/ancestral_seqs_ML_predict_living_seqs_nucleotide.mao
+echo $mao
 aln=/home/tuf79348/ep_env/$val_58.fas
+echo $aln
 tree=/home/tuf79348/ep_env/$val_58.nwk
+echo $tree
 output=/home/tuf79348/ep_env/$val_58.csv
+echo $output
 
 srun echo running megacc calculations for $val
 srun /home/tuf79348/ep_env/megacc -a $mao -d $aln -t $tree -o $output
